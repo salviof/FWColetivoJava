db15f7052b947e8eb8d23f5fe22e35f7265ff1c0
Salvio Furbino Elias <salviof@gmail.com>
2019-01-26 00:14:27 -0200

    Atualizacao automática

---
+++ baixarDependencias.sh
@@ -0,0 +1,16 @@
+mkdir /home/superBits/projetos/coletivoJava/source/fw -p
+cd /home/superBits/projetos/coletivoJava/source/
+git clone https://github.com/salviof/reflections.git
+cd 
+/home/superBits/projetos/coletivoJava/source/fw
+git clone https://github.com/salviof/SBApiCore.git
+git clone https://github.com/salviof/SBCore.git
+git clone https://github.com/salviof/SBTeste.git
+git clone https://github.com/salviof/util.git
+        
+git clone https://github.com/salviof/SBPersistencia.git
+git clone https://github.com/salviof/SBAcessosModel.git
+   
+mkdir /home/superBits/projetos/coletivoJava/source/erpColetivoJava/ -p 
+git clone https://github.com/salviof/apiComunicacao.git        
+git clome https://github.com/salviof/implementacaoPadraoComunicacao.git
---
+++ dependenciasErpOpen.sh
@@ -0,0 +1,19 @@
+mkdir /home/superBits/projetos/Super_Bits/source/ -p
+cd /home/superBits/projetos/Super_Bits/source/
+
+cd /home/superBits/projetos/Super_Bits/source/
+git clone https://github.com/salviof/Controle_Usuario_Basico
+cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico
+source /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava/instalarModulo.sh
+
+cd /home/superBits/projetos/Super_Bits/source/
+git clone https://github.com/salviof/mktMauticIntegracao
+cd /home/superBits/projetos/Super_Bits/source/mktMauticIntegracao
+source /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava/instalarModulo.sh
+
+cd /home/superBits/projetos/Super_Bits/source/
+git clone https://github.com/salviof/SB_AdminTools
+cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools
+source /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava/instalarModulo.sh
+cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/webApp
+./empacotarModulo
