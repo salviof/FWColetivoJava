4f1c8cc60c07990fc8ae558d413df5d040cd418b
Victor Novy <novyvictor@gmail.com>
2019-02-10 18:08:15 -0200

    Atualizacao automática

---
+++ enviarAlteracoes.sh
@@ -0,0 +1,22 @@
+cd /home/superBits/projetos/coletivoJava/source/fw/SBApiCore/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/SBCore/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/SBTeste/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/SBPersistencia/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/util/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/SBWebPaginas/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/apiComunicacao/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/SBAcessosModel/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava/
+./sincronizaGit.sh
+cd /home/superBits/projetos/coletivoJava/source/erpColetivoJava/SBErpGatewayPagamento/
+./sincronizaGit.sh
+cd cd /home/superBits/projetos/coletivoJava/source/erpColetivoJava/apiComunicacao/
+./sincronizaGit.sh
