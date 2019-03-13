90cb247c5ed9b72fe684bac139aba12f9d55a9d2
Salvio Furbino Elias <salviof@gmail.com>
2019-03-13 13:41:04 -0300

    Atualizacao automática

--- dependenciasErpOpen.sh
+++ dependenciasErpOpen.sh
@@ -1,9 +1,11 @@
 mkdir /home/superBits/projetos/Super_Bits/source/ -p
 cd /home/superBits/projetos/Super_Bits/source/
 git clone https://github.com/salviof/Controle_Usuario_Basico
-git clone https://github.com/salviof/mktMauticIntegracao
+
 git clone https://github.com/salviof/SB_AdminTools
 git clone https://github.com/salviof/SBProjetos
 mkdir /home/superBits/projetos/coletivoJava/source/erpColetivoJava -p
 cd /home/superBits/projetos/coletivoJava/source/erpColetivoJava
 git clone https://github.com/salviof/apiComunicacao.git
+git clone https://github.com/salviof/mktMauticIntegracao
+
