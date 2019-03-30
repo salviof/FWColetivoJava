bae5cda4043b44602444ef5c637c8e24d6249eba
Salvio Furbino <salviof@gmail.com>
2019-03-30 13:08:07 -0300

    Atualizacao automática

--- compilarTudo.sh
+++
@@ -1,7 +0,0 @@
-
-cd /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava
-mvn clean install -DskipTests=true
-cd /home/superBits/projetos/coletivoJava/source/fw/FWColetivoJava/ferramentasDev
-mvn clean install -DskipTests=true
-
-
