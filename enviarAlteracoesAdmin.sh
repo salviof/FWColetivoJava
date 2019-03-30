a558ad41babcdc42b4136a5ebca8aac385e99f99
Salvio Furbino <salviof@gmail.com>
2019-03-30 13:17:12 -0300

    Atualizacao automática

---
+++ enviarAlteracoesAdmin.sh
@@ -0,0 +1,8 @@
+cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico/
+./sincronizaGit.sh
+cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/
+./sincronizaGit.sh
+cd /home/superBits/projetos/Super_Bits/source/SB_CRIADOR_COMPONENTE/
+./sincronizaGit.sh
+cd /home/superBits/projetos/Super_Bits/source/SBProjetos/
+./sincronizaGit.sh
