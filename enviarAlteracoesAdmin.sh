a22a4d8dd63ab9e7f63d646cd75bfb332666e49b
Salvio Furbino <salviof@gmail.com>
2020-08-11 01:11:07 -0300

    Atualizacao automática

--- enviarAlteracoesAdmin.sh
+++
@@ -1,8 +0,0 @@
-cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico/
-./sincronizaGit.sh
-cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/
-./sincronizaGit.sh
-cd /home/superBits/projetos/Super_Bits/source/SB_CRIADOR_COMPONENTE/
-./sincronizaGit.sh
-cd /home/superBits/projetos/Super_Bits/source/SBProjetos/
-./sincronizaGit.sh
--- instalacaoAutomatica.sh
+++ instalacaoAutomatica.sh
@@ -9,11 +9,14 @@ cd /home/superBits/superBitsDevOps
 ./dependenciasAplicativosBase.sh
 ./baixarDependencias.sh
 ./compilarTudo.sh
-cat /home/superBits/logoAsci.txt
 echo "Se você não viu nenhum erro de compilação,As dependencias foram instaladas corretamente!"
 echo "Para instalar e configurar o Java8 e o Netbens execute"
 echo "execute /home/superBits/superBitsDevOps/estacaoDeveloperOps/instalaAplicativos.sh"
 echo "Para configurar o Git, execute /home/superBits/superBitsDevOps/estacaoDeveloperOps/prepararEstacao.sh"
+echo "execute  /home/superBits/SBConsole.sh para abrir este programa"
+./compilaExecutaColetivoJavaCli.sh
+
+
 
 
 
